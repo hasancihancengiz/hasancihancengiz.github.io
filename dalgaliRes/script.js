aa8944e719a1b83b747c5dee0c58aeb04cacb8a8
hasancihancengiz <hasancihancengiz@gmail.com>
2022-02-10 21:15:22 +0300

    anim

---
+++ dalgaliRes/script.js
@@ -0,0 +1,41 @@
+const header = document.getElementById("header");
+console.log(header);
+var order = 0;
+let strchng = order.toString()
+
+console.log(strchng);
+
+
+header.style.backgroundImage = "url(assets/" + strchng + ".jpg)";
+
+console.log(header.style.backgroundImage)
+
+//const rez = document.getElementsByClassName("rez")
+const rez = document.getElementsByTagName("h1");
+
+setInterval(function () {
+    order++;
+    strchng = order.toString();
+    header.style.backgroundImage = "url(assets/" + strchng + ".jpg)";
+
+    if(order>=3){
+        order = 0;
+    }}, 2000);
+
+
+
+
+
+rez[0].addEventListener("click", function(){
+    order++;
+    strchng = order.toString();
+    header.style.backgroundImage = "url(assets/" + strchng + ".jpg)";
+
+    if(order>=3){
+        order = 0;
+    }
+});
+
+
+console.log(rez)
+
