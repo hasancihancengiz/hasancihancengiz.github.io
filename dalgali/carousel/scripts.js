aa8642db87dac9967721d97f7827094bed357486
hasancihancengiz <hasancihancengiz@gmail.com>
2022-02-13 17:54:51 +0300

    carousel1

---
+++ dalgali/carousel/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:1,
+        loop:true,
+        margin:0,
+        autoplay:true,
+        autoplayTimeout:2500,
+        autoplayHoverPause:true
+    });
+  });
